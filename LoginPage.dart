1a28c8c3c75bfbdd568583bbf863882bc133094a
Nishant Gupta <gupta.nishant08042004@gmail.com>
2024-09-20 01:15:35 +0530

    Create LoginPage.dart

---
+++ LoginPage.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Login',
+          style: TextStyle(color: Colors.white), // Set the title text color to white
+        ),
+        backgroundColor: Colors.black, // Background color of the AppBar
+        iconTheme: IconThemeData(color: Colors.white), // Set the back button color to white
+      ),
+      body: Center(
+        child: Text(
+          'Login Page Content Goes Here',
+          style: TextStyle(fontSize: 24, color: Colors.black), // Set the content text color to white
+        ),
+      ),
+    );
+  }
+}
